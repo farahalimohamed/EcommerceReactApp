8a62e8d5ced2878a65ee850fdc1d3be5be8bb290
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-01-23 01:04:24 +0200

    implement register functionality

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,8 +1,61 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "./Register.module.css";
+import { useFormik } from "formik";
+import axios from "axios";
+import * as Yup from "yup";
+import { useState } from "react";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 export default function Register() {
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+    rePassword: "",
+    phone: "",
+  };
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .required("Name is required")
+      .min(3, "Name must be at least 3 characters")
+      .max(20, "Name must be at most 20 characters"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    phone: Yup.string()
+      .required("Phone is required")
+      .matches(/^01[0125][0-9]{8}$/, "Invalid phone number"),
+    password: Yup.string()
+      .required("Password is required")
+      .matches(/^[A-Za-z1-9]{6,10}$/, "Password must be 6-10 characters"),
+    rePassword: Yup.string()
+      .required("Confirm password is required")
+      .oneOf([Yup.ref("password"), null], "Passwords must match"),
+  });
+  async function handleRegister(data) {
+    setIsLoading(true);
+    const response = await axios
+      .post("https://ecommerce.routemisr.com/api/v1/auth/signup", data)
+      .then((res) => {
+        setErrorMsg(null);
+        setIsLoading(false);
+        navigate("/login");
+      })
+      .catch((err) => {
+        setErrorMsg(err.response.data.message);
+        setIsLoading(false);
+      });
+  }
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleRegister,
+  });
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-50 dark:bg-gray-900 pb-10 pt-5">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="flex items-center mb-6 text-3xl font-semibold text-gray-900 dark:text-white">
           Register Now
@@ -12,7 +65,10 @@ export default function Register() {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Create an account
             </h1>
-            <form className="space-y-4 md:space-y-6">
+            <form
+              className="space-y-4 md:space-y-6"
+              onSubmit={formik.handleSubmit}
+            >
               <div>
                 <label
                   htmlFor="name"
@@ -21,12 +77,18 @@ export default function Register() {
                   Your name
                 </label>
                 <input
-                  type="name"
+                  type="text"
                   name="name"
                   id="name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Enter your name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
+                {formik.errors.name && formik.touched.name && (
+                  <small className="text-red-600">{formik.errors.name}</small>
+                )}
               </div>
               <div>
                 <label
@@ -41,7 +103,34 @@ export default function Register() {
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
+                {formik.errors.email && formik.touched.email && (
+                  <small className="text-red-600">{formik.errors.email}</small>
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="phone"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your phone
+                </label>
+                <input
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="e.g. +20 125 678 9104"
+                  value={formik.values.phone}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.errors.phone && formik.touched.phone && (
+                  <small className="text-red-600">{formik.errors.phone}</small>
+                )}
               </div>
               <div>
                 <label
@@ -56,7 +145,15 @@ export default function Register() {
                   id="password"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
+                {formik.errors.password && formik.touched.password && (
+                  <small className="text-red-600">
+                    {formik.errors.password}
+                  </small>
+                )}
               </div>
               <div>
                 <label
@@ -66,19 +163,41 @@ export default function Register() {
                   Confirm password
                 </label>
                 <input
-                  type="rePassword"
+                  type="password"
                   name="rePassword"
                   id="rePassword"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={formik.values.rePassword}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
+                {formik.errors.rePassword && formik.touched.rePassword && (
+                  <small className="text-red-600">
+                    {formik.errors.rePassword}
+                  </small>
+                )}
               </div>
-              <button
-                type="submit"
-                className="w-full text-white bg-[#0AAD0A] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Create an account
-              </button>
+              {errorMsg && (
+                <div className="bg-red-300 p-3 rounded-md">{errorMsg}</div>
+              )}
+              {isLoading ? (
+                <button
+                  type="submit"
+                  disabled
+                  className="w-full text-white text-center flex justify-center items-center bg-[#0AAD0A] disabled:bg-[#87d387] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  <AiOutlineLoading3Quarters className="animate-spin" />
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  className="w-full text-white bg-[#0AAD0A] disabled:bg-[#87d387] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  disabled={!formik.isValid && formik.dirty}
+                >
+                  Create an account
+                </button>
+              )}
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account?{" "}
                 <Link
