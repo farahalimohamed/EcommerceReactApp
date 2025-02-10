a15b1a84fce72395e91b0cc3cea8478fa02b5d7b
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-02-10 03:14:44 +0200

    handle category and responsive slider

--- src/Components/CategorySlider/CategorySlider.jsx
+++ src/Components/CategorySlider/CategorySlider.jsx
@@ -1,6 +1,81 @@
-import styles from './CategorySlider.module.css'
+import axios from "axios";
+import styles from "./CategorySlider.module.css";
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2,
+        initialSlide: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
 export default function CategorySlider() {
+  const [categories, setCategories] = useState([]);
+  async function getCategories() {
+    await axios
+      .get("https://ecommerce.routemisr.com/api/v1/categories")
+      .then((response) => {
+        setCategories(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
-    <div>CategorySlider</div>
-  )
+    <div className="my-8 mx-10">
+      <Slider {...settings}>
+        {categories.map((category) => (
+          <div key={category._id}>
+            {console.log(category)}
+            <img
+              src={category.image}
+              alt={category.name}
+              className="w-full h-[300px]"
+            />
+            <h4 className="m-3 font-semibold">{category.name}</h4>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import CategorySlider from '../../Components/CategorySlider/CategorySlider'
 import LatestProducts from '../../Components/LatestProducts/LatestProducts'
 import styles from './Home.module.css'
 export default function Home() {
   return (
     <div className='container'>
+      <CategorySlider/>
       <LatestProducts/>
     </div>
   )
