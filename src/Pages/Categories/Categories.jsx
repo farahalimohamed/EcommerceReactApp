231c248079593e1c68ca3e41591857b77a416194
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-02-24 22:51:20 +0200

    ui updates

--- src/Pages/Categories/Categories.jsx
+++ src/Pages/Categories/Categories.jsx
@@ -1,46 +1,59 @@
-import { useEffect, useState } from 'react';
-import styles from './Categories.module.css'
-import axios from 'axios';
-import { Helmet } from 'react-helmet';
+import { useEffect, useState } from "react";
+import styles from "./Categories.module.css";
+import axios from "axios";
+import { Helmet } from "react-helmet";
+import Loader from "../../Components/Loader/Loader";
+
 export default function Categories() {
   const [categories, setCategories] = useState([]);
-    async function getCategories() {
-      try {
-        const response = await axios.get(
-          "https://ecommerce.routemisr.com/api/v1/categories"
-        );
-        setCategories(response.data.data);
-      } catch (error) {
-        console.error("Error fetching categories:", error);
-      }
+  const [isLoading, setIsLoading] = useState(true); 
+
+  async function getCategories() {
+    try {
+      const response = await axios.get(
+        "https://ecommerce.routemisr.com/api/v1/categories"
+      );
+      setCategories(response.data.data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    } finally {
+      setIsLoading(false); 
     }
-    useEffect(() => {
-      getCategories();
-    }, [])
-    
+  }
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
     <section className="py-8">
       <Helmet>
         <title>Categories</title>
       </Helmet>
       <div className="mx-auto grid max-w-screen-xl grid-cols-2 gap-8 text-gray-500 dark:text-gray-400 sm:grid-cols-3 sm:gap-12 lg:grid-cols-6 px-4">
-        {categories.map((category) => (
+        {isLoading ? (
+          <div className="col-span-full flex justify-center items-center h-screen">
+            <Loader />
+          </div>
+        ) : (
+          categories.map((category) => (
             <div
               key={category._id}
-              class="max-w-sm bg-white rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
+              className="max-w-sm bg-white rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
             >
               <img
-                class="rounded-t-lg h-[200px] object-center w-full"
+                className="rounded-t-lg h-[200px] object-center w-full"
                 src={category.image}
                 alt={category.name}
               />
-              <div class="p-5 h-[90px] border-b border-r border-l border-gray-200 rounded-b-lg">
-                <p class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <div className="p-5 h-[90px] border-b border-r border-l border-gray-200 rounded-b-lg">
+                <p className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
                   {category.name}
                 </p>
               </div>
             </div>
-        ))}
+          ))
+        )}
       </div>
     </section>
   );
