a656f588fa94d8a983512988ec6dca76666f151c
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2025-02-24 04:36:04 +0200

    add animation when add to wishlist

--- src/Components/ProductItem/ProductItem.jsx
+++ src/Components/ProductItem/ProductItem.jsx
@@ -1,7 +1,17 @@
-import { FaStar } from "react-icons/fa";
+import { FaHeart, FaRegHeart, FaStar } from "react-icons/fa";
 import styles from "./ProductItem.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 export default function ProductItem({ product, addProductToCart, addProductToWishlist }) {
+    const [isInWishlist, setIsInWishlist] = useState(false);
+
+    const handleAddToWishlist = async (productId) => {
+      setIsInWishlist(true); 
+      await addProductToWishlist(productId); 
+      setTimeout(() => {
+        setIsInWishlist(false);
+      }, 2000); 
+    };
   return (
     <div className="bg-white flex flex-col overflow-hidden cursor-pointer hover:shadow-md transition-all">
       <Link to={`/productdetails/${product._id}`}>
@@ -48,21 +58,17 @@ export default function ProductItem({ product, addProductToCart, addProductToWis
         </div>
         <div className="flex items-center gap-2 mt-4">
           <button
-            onClick={() => addProductToWishlist(product._id)}
-            className="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer"
+            onClick={() => handleAddToWishlist(product._id)}
+            className={`bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer ${
+              isInWishlist ? "animate-pulse" : ""
+            }`}
             title="Wishlist"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16px"
-              className="fill-pink-600 inline-block"
-              viewBox="0 0 64 64"
-            >
-              <path
-                d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                data-original="#000000"
-              />
-            </svg>
+            {isInWishlist ? (
+              <FaHeart className="text-pink-600" />
+            ) : (
+              <FaRegHeart className="text-pink-600" />
+            )}
           </button>
           <button
             onClick={() => addProductToCart(product._id)}
